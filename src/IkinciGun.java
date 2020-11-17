0585c7d12d8530aa1d32e4bda8ddb515fff3495f
firdevskaya <74245732+firdevskaya@users.noreply.github.com>
2020-11-17 23:24:32 +0300

    Update IkinciGun.java

--- src/IkinciGun.java
+++ src/IkinciGun.java
@@ -5,11 +5,11 @@ public class IkinciGun {
 		// TODO Auto-generated method stub
 
 		System.out.println("merhaba");
-		System.out.println("10:11 - İkinci Gün");
-		System.out.println("10:35 - İkinci Gün");
+		System.out.println("10:11 - İkinci Gun");
+		System.out.println("10:35 - İkinci Gun");
 
 		System.out.println("11.05-local");
-
+System.out.println("11.23-github.com");
 		
 
 		
