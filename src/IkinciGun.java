05ec409d02940328f6f4aa95b65ef4def08cdc3f
firdevskaya <ffirdevskaya31@gmail.com>
2020-11-18 23:46:38 +0300

    mesaj

--- src/IkinciGun.java
+++ src/IkinciGun.java
@@ -15,7 +15,7 @@ System.out.println("merhaba");
 	         System.out.println("feature deneme");
 	System.out.println(" yeni deneme 123");
 	System.out.println(" 11-20 feature branch ");
-
+System.out.println("11 45 feature branch ");
 	}
 }
 
