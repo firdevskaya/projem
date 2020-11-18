1d1e7ac33992bc6b164fb0eabcc73351a92b2bfb
firdevskaya <ffirdevskaya31@gmail.com>
2020-11-18 22:54:43 +0300

    feature

--- src/IkinciGun.java
+++ src/IkinciGun.java
@@ -2,9 +2,7 @@
 public class IkinciGun {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		System.out.println("merhaba");
+System.out.println("merhaba");
 		System.out.println("10:11 - İkinci Gun");
 		System.out.println("10:35 - İkinci Gun");
 
@@ -12,7 +10,11 @@ public class IkinciGun {
         System.out.println("11.23-github.com");
         System.out.println("ben burdan conflit beklemiyorum");	
              System.out.println("localde yeni bir satir");
-
+	System.out.println("10. 26- feature branch");
+             System.out.println("feature deneme");
+	
+	
+	
 	}
 }
 
